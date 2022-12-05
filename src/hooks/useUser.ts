4fc4697bf83872617505af55b9feb11b0d813aa8
Insoo-Seo <kocon135@gmail.com>
2022-12-06 00:18:35 +0900

    [fix] user Interface 및 user context 변수명 수정

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -2,11 +2,11 @@ import { useContext } from 'react';
 import { UserActionsContext, UserStateContext } from '../context/user';
 
 const useUser = () => {
-  const user = useContext(UserStateContext);
+  const userInfo = useContext(UserStateContext);
   const userAction = useContext(UserActionsContext);
 
   return {
-    user,
+    userInfo,
     userAction,
   };
 };
