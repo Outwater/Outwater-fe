9b41186990009cbbc44922f257bda6790985efb5
Insoo-Seo <kocon135@gmail.com>
2022-12-07 17:46:09 +0900

    [feat] Intersection Observer를 통한 무한스크롤 구현

--- src/pages/infinite-scroll.tsx
+++ src/pages/infinite-scroll.tsx
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/router';
 import type { NextPage } from 'next';
 import styled from 'styled-components';
 
-import products from '../api/data/products.json';
+import { Product } from '../types/product';
+import { request } from '../api/request';
+import useIntersection from '../hooks/useInterection';
 import ProductList from '../components/ProductList';
 import Layout from '../components/Layout';
 
 const InfiniteScrollPage: NextPage = () => {
+  const router = useRouter();
+
+  const [products, setProducts] = useState<Product[]>([]);
+  const [totalCnt, setTotalCnt] = useState<Number | null>(null);
+  const [page, setPage] = useState(1);
+  const ITEM_COUNT_PER_FETCH = 16;
+
+  const fetchMoreRef = useRef<HTMLDivElement | null>(null);
+  const intersecting = useIntersection(fetchMoreRef);
+
+  useEffect(() => {
+    if (intersecting) {
+      fetchMore();
+    }
+  }, [intersecting]);
+
+  const fetchMore = () => {
+    if (totalCnt === products.length) {
+      alert('모든 상품을 불러왔습니다!');
+      return;
+    }
+
+    const fetchProducts = async (page: number, size: number) => {
+      try {
+        const { data } = await request(`/products?page=${page}&size=${size}`);
+        const newProducts = [...products, ...data.products];
+        setProducts(newProducts);
+        setTotalCnt(data.totalCount);
+      } catch (err: any) {
+        err.name === 'NotFound' && router.push('/404');
+      }
+    };
+    fetchProducts(page, ITEM_COUNT_PER_FETCH);
+    setPage((page) => page + 1);
+  };
+
   return (
     <Layout>
       <Container>
         <ProductList products={products} />
+        <FetchMore ref={fetchMoreRef} />
       </Container>
     </Layout>
   );
@@ -23,4 +63,8 @@ const Container = styled.main`
   flex-direction: column;
   align-items: center;
   padding: 0 20px 40px;
+`;
+
+const FetchMore = styled.div`
+  visibility: hidden;
 `;
